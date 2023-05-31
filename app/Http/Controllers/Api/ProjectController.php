93f7684a6c676b098d84735436281dce92329deb
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-05-31 15:09:02 +0200

    bonus complete

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,17 +4,40 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
 
-        $projects = Project::with('type', 'technologies')->paginate(4);
+        $requestData = $request->all();
+
+        $types = Type::all();
+
+        if($request->has('type_id') && $requestData['type_id']) {
+
+            $projects = Project::where('type_id', $requestData['type_id'])
+                ->with('type', 'technologies')
+                ->paginate(4);
+        
+            if(count($projects) == 0) {
+                return response()->json([
+                    'success' => false,
+                    'error' => 'No projects found with this type'
+                ]);
+            }
+        } else {
+
+            $projects = Project::with('type', 'technologies')->paginate(4);
+            
+
+        }
 
         return response()->json([
             'success' => true,
             'results' => $projects,
+            'allTypes' => $types,
         ]);
 
     }
